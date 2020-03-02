59f7c54062a11443211fa7e3df187955e3f5a44a
Jonas Volken <jonas.volken@gmail.com>
2020-03-02 11:27:32 +0100

    implement generic portfolio

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,43 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+import portfolio.investments.Investment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Portfolio<T extends Investment> {
+
+    private Map<String, T> investments;
+
+    public Portfolio() {
+        this.investments = new HashMap<>();
+    }
+
+    public boolean contains(T investment) {
+        return investments.containsKey(investment.getTitel());
+    }
+
+    public void buy(T investment) {
+        if (investments.containsKey(investment.getTitel())) {
+            investment = investments.get(investment.getTitel());
+            investment.setCount(investment.getCount() + 1);
+            return;
+        }
+        investments.put(investment.getTitel(), investment);
+    }
+
+    public void sell(String name, int amount) {
+        if(!investments.containsKey(name)){
+           return;
+        }
+        T investment = investments.get(name);
+        if( investment.getCount() <= amount){
+            investments.remove(name);
+        }
+        investment.setCount(investment.getCount() - amount);
+    }
+
+    public T getShare(String name) {
+        return investments.get(name);
+    }
 }
